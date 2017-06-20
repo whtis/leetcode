a1de727b1c318fdef55f19d51b27c5b0ca0b57c2
wuhaitao <me@whtis.com>
2017-06-20 09:15:24 +0800

    2016.6.19 打卡

---
+++ src/easy/HashTable/E594.java
@@ -0,0 +1,23 @@
+package easy.HashTable;
+
+import java.util.HashMap;
+
+/**
+ * Created by wht on 2017/6/19.
+ */
+public class E594 {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key + 1) + map.get(key));
+            }
+        }
+        return result;
+
+    }
+}
---
+++ src/easy/HashTable/E624.java
@@ -0,0 +1,8 @@
+package easy.HashTable;
+
+/**
+ * Created by wht on 2017/6/18.
+ */
+public class E624 {
+
+}
